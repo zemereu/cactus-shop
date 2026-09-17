b46691cca05b22c95cadfd90f3f0cecb356a95a9
Denes Eduard <eduarddenes129@gmail.com>
2026-09-17 11:22:11 +0300

    account button fix + cont.html redesign

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,7 @@ function showToast(message: string) {
     if (!container) return;
 
     const toast = document.createElement('div');
-    toast.innerText = message;
+    toast.innerHTML = message;
     toast.style.backgroundColor = "#2f694b"; // Noul verde
     toast.style.color = "#fdf2b8"; // Noul crem pentru text
     toast.style.padding = "15px 25px";
